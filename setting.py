89cd117dbc32ad5cd4b70bc26c79c189a6927aab
huybaoash <huybaoash@gmail.com>
2024-07-27 01:19:21 +0700

    update

--- setting.py
+++ setting.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-db_url = os.getenv('DATABASE_URL')
+db_url = 'postgresql://capstone_j2la_user:F3jzjN8RJCcUTkXdJfmFgfxzmswLaIBs@dpg-cqhub40gph6c73capdag-a/capstone_j2la'
 
 db_name = os.getenv('DB_NAME')
 db_user = os.getenv('DB_USER')
@@ -11,5 +11,5 @@ db_host = os.getenv('DB_HOST')
 db_port = os.getenv('DB_PORT')
 
 
-auth0_domain = os.getenv('AUTH0_DOMAIN')
-api_audience = os.getenv('API_AUDIENCE')
+auth0_domain = 'dev-kyu5aghzid56evr1.us.auth0.com'
+api_audience = 'capstone-udacity'
---
+++ setup.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+export DATABASE_URL="postgresql://postgres@localhost:5432/postgres"
+export EXCITED="true"
+echo "setup.sh script executed successfully!"
